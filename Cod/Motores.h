cd7550dcf51ab21c751206ff6bbfd12f55201ed4
PauloVLB <vitorlimaborges@gmail.com>
2018-03-19 10:37:13 -0300

    Erros consertados

--- Cod/Motores.h
+++ Cod/Motores.h
@@ -1,9 +1,10 @@
 #include <robo_hardware.h>
 
-class Movimentacao {
+class Motores {
 	public:
 		void emFrente(int velEsq, int velDir){robo.acionarMotores(velEsq, velDir);}
 		void direita(int velEsq, int velDir){robo.acionarMotores(velEsq, velDir*(-1));}   // Delirante
 		void esquerda(int velEsq, int velDir){robo.acionarMotores(velEsq*(-1), velDir);}  // Com fome
 		void paraTras(int velEsq, int velDir){robo.acionarMotores(velEsq*(-1), velDir*(-1));}
+		void parar(int tempo){robo.acionarMotores(0, 0); delay(tempo);}
 };
