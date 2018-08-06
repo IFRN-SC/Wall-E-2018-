5505b8b10177e4080d36cdb7854cca561b721aa7
isaac <isaacmarlondasilvalourenco@gmail.com>
2018-08-06 18:13:19 -0300

    Ajustes na classe Garra

--- Cod/Garra.cpp
+++ Cod/Garra.cpp
@@ -1,44 +1,44 @@
 #include "Garra.h"
 
-void Garra::subir(int ang_inicial, int ang_final, int tempo) {
+void Garra::subir() {
 
-	for(int i = ang_inicial; i >= ang_final; i++){
-
-		robo.acionarServoGarra1(i);
-		delay(tempo);
-
-	}
-
-}
-
-void Garra::baixar(int ang_inicial, int ang_final, int tempo) {
-
-	for(int i = ang_inicial; i >= ang_final; i--){
+	for(int i = ANG_INIT; i >= 0; i++){
 
 		robo.acionarServoGarra1(i);
-		delay(tempo);
+		delay(TEMPO);
 
 	}
 
 }
 
-void Garra::abrir(int ang_inicial, int ang_final, int tempo) {
+void Garra::baixar() {
 
-	for(int i = ang_inicial; i >= ang_final; i--){
+	for(int i = ANG_INIT; i >= 0; i--){
 
-		robo.acionarServoGarra2(i);
-		delay(tempo);
+		robo.acionarServoGarra1(i);
+		delay(TEMPO);
 
 	}
 
 }
 
-void Garra::fechar(int ang_inicial, int ang_final, int tempo) {
+void Garra::abrir() {
 
-	for(int i = ang_inicial; i >= ang_final; i++){
+	for(int i = ANG_INIT; i >= 0; i--){
 
 		robo.acionarServoGarra2(i);
-		delay(tempo);
+		delay(TEMPO);
+
+	}
+
+}
+
+void Garra::fechar() {
+
+	for(int i = 0; i >= ANG_INIT; i++){
+
+		robo.acionarServoGarra2(i);
+		delay(TEMPO);
 
 	}
 
--- Cod/Garra.h
+++ Cod/Garra.h
@@ -3,20 +3,25 @@
 
 #include <robo_hardware2.h>
 
+#define ANG_INIT 90
+
+#define TEMPO 20
+
 class Garra {
 
 	public:
+		/*
 		void iniciar() { 
 
 			robo.acionarServoGarra1(90); 
 			robo.acionarServoGarra2(90);
 
-		} 
+		}*/
 
-		void subir(int ang_inicial, int ang_final, int tempo);
-		void baixar(int ang_inicial, int ang_final, int tempo);
-		void abrir(int ang_inicial, int ang_final, int tempo);
-		void fechar(int ang_inicial, int ang_final, int tempo);
+		void subir();
+		void baixar();
+		void abrir();
+		void fechar();
 	
 };
 
