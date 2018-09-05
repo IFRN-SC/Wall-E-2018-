27806a57dcdd9774d19cfd178c6d28b9fa63d3d9
Isaac Marlon <isaacmarlondasilvalourenco@gmail.com>
2018-09-04 21:56:48 -0300

    Ajustes na leitura dos dados da EEPROM

--- Cod/Calibracao2.cpp
+++ Cod/Calibracao2.cpp
@@ -397,14 +397,26 @@ void Calibracao2::Menu_calibrar() {
 	  corEsquerdo.setVerde(cali.verdeEsq);
 	  corDireito.setVerde(cali.verdeDir);
 
-	  Serial.println("mais esq, esq, dir, mais dir");
+
+	  Serial.println(F("\nSENSORES:\n"));
+	  Serial.println(F("(maisEsq)	---		(Esq)		---		(Dir)		---		(maisDir)\n"));
 	  Serial.println(refletancia_esq2.getSeparacao());
+	  Serial.print(F("		---		"));
 	  Serial.println(refletancia_esq.getSeparacao());
+	  Serial.print(F("		---		"));
 	  Serial.println(refletancia_dir.getSeparacao());
+	  Serial.print(F("		---		"));
 	  Serial.println(refletancia_dir2.getSeparacao());
 
 	  sala3.setLimiteLateral(cali.sala3_limiteLateral);
 	  sala3.setLimiteFrontal(cali.sala3_limiteFrontal);
+
+	  Serial.println(F("\nSALA3:\n"));
+	  Serial.println(F("(limiteLateral)	---		(limiteFrontal)\n"));
+	  Serial.println(sala3.getLimiteLateral());
+	  Serial.print(F("		---		"));
+	  Serial.println(sala3.getLimiteFrontal());
+
 
  }
  
