c99669ecc2f0bd85329b01d3d1aa02b6c9304fe9
PauloVLB <vitorlimaborges@gmail.com>
2018-09-28 21:57:31 -0300

    Classe garra atualizada

--- Cod/Garra.h
+++ Cod/Garra.h
@@ -9,13 +9,13 @@ class Garra {
 		#define DELAY 20
 		
 		#define ANG_INICIAL_SUBIR_BRACO 10
-		#define ANG_FINAL_SUBIR_BRACO 80
+		#define ANG_FINAL_SUBIR_BRACO 180
 
 		#define ANG_INICIAL_BAIXAR_BRACO 80
-		#define ANG_FINAL_BAIXAR_BRACO 10
+		#define ANG_FINAL_BAIXAR_BRACO 90
 
 		#define ANG_INICIAL_ABRIR_MAO 90
-		#define ANG_FINAL_ABRIR_MAO 0
+		#define ANG_FINAL_ABRIR_MAO 180
 
 		#define ANG_INICIAL_FECHAR_MAO 0
 		#define ANG_FINAL_FECHAR_MAO 90
@@ -30,10 +30,10 @@ class Garra {
 		void setMao(int angInicial, int angFinal, int tempo);
 		void setBraco(int angInicial, int angFinal, int tempo);
 		
-		void subir();
-		void baixar();
-		void abrir();
-		void fechar();
+		inline void subir()  { robo.acionarServoGarra2(ANG_FINAL_SUBIR_BRACO, DELAY); }
+		inline void baixar() { robo.acionarServoGarra2(ANG_FINAL_BAIXAR_BRACO, DELAY); }
+		inline void abrir()  { robo.acionarServoGarra1(ANG_FINAL_ABRIR_MAO, DELAY); }
+		inline void fechar() { robo.acionarServoGarra1(ANG_FINAL_FECHAR_MAO, DELAY); }
 	
 };
 
