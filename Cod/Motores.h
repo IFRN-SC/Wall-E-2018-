788d4b7b64a8896401b166dd567e27f6695ae7a6
PauloVLB <vitorlimaborges@gmail.com>
2018-10-12 17:50:28 -0300

    Classe motores atualizada para a versão mais atualizada.

--- Cod/Motores.h
+++ Cod/Motores.h
@@ -5,14 +5,28 @@
 
 class Motores {
 	private:
-		#define VEL_FRENTE 40
-		#define VEL_TRAS 40
+		#define VEL_DIR_FRENTE_RAMPA 92
+		#define VEL_DIR_TRAS_RAMPA 70
+
+		#define VEL_ESQ_FRENTE_RAMPA 85
+		#define VEL_ESQ_TRAS_RAMPA 70
+
+		#define VEL_DIR_FRENTE 57
+		#define VEL_DIR_TRAS -52
+
+		#define VEL_ESQ_FRENTE 50
+		#define VEL_ESQ_TRAS -50
 	public:
-		void emFrente(int velEsq, int velDir){robo.acionarMotores(velEsq, velDir);}
-		void direita(int velEsq, int velDir){robo.acionarMotores(velEsq, velDir*(-1));}   // Delirante
-		void esquerda(int velEsq, int velDir){robo.acionarMotores(velEsq*(-1), velDir);}  // Com fome
-		void paraTras(int velEsq, int velDir){robo.acionarMotores(velEsq*(-1), velDir*(-1));}
-		void parar(int tempo){robo.acionarMotores(0, 0); delay(tempo);}
+		inline void emFrente(){robo.acionarMotores(VEL_ESQ_FRENTE, VEL_DIR_FRENTE);}
+		inline void direita(){robo.acionarMotores(VEL_ESQ_FRENTE, VEL_DIR_TRAS);} 
+		inline void esquerda(){robo.acionarMotores(VEL_ESQ_TRAS, VEL_DIR_FRENTE);}  
+		inline void paraTras(){robo.acionarMotores(VEL_ESQ_TRAS, VEL_DIR_TRAS);}
+
+		inline void emFrenteRampa(){robo.acionarMotores(VEL_ESQ_FRENTE_RAMPA, VEL_DIR_FRENTE_RAMPA);}
+		inline void direitaRampa(int decremento=0){robo.acionarMotores(VEL_ESQ_FRENTE_RAMPA, VEL_DIR_TRAS_RAMPA-decremento);} 
+		inline void esquerdaRampa(int decremento=0){robo.acionarMotores(VEL_ESQ_TRAS_RAMPA-decremento, VEL_DIR_FRENTE_RAMPA);}  
+
+		inline void parar(int tempo){robo.acionarMotores(0, 0); delay(tempo);}
 };
 
 #endif
